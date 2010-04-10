144c075ce243601241143bd58f868a60f9bd330d
Ryan Petrich <rpetrich@gmail.com>
2010-04-10 17:48:03 -0600

    Define IOSURFACE_API_FALLBACK so that projects can detect if fallback headers are being used

--- _fallback/IOSurface/IOSurfaceAPI.h
+++ _fallback/IOSurface/IOSurfaceAPI.h
@@ -1,5 +1,6 @@
 #ifndef _IOSURFACE_API_H
 #define _IOSURFACE_API_H 1
+#define IOSURFACE_API_FALLBACK 1
 
 // Allow compilation of most projects even if non-free header is missing
 typedef struct __IOSurface *IOSurfaceRef;
