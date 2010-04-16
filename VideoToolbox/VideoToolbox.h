00784f189a43d9ae46c8a48996299ea9125337cd
Ryan Petrich <rpetrich@gmail.com>
2010-04-15 22:44:15 -0600

    Added start of VideoToolbox; CVPixelBuffer fallback

---
+++ VideoToolbox/VideoToolbox.h
@@ -0,0 +1,1 @@
+#include "VTPixelTransferSession.h"
---
+++ _fallback/CoreVideo/CVPixelBuffer.h
@@ -0,0 +1,8 @@
+#ifndef __COREVIDEO_CVPIXELBUFFER_H__
+#define __COREVIDEO_CVPIXELBUFFER_H__ 1
+#define CVPIXELBUFFER_FALLBACK 1
+
+typedef int32_t CVReturn;
+typedef struct __CVPixelBuffer *CVPixelBufferRef;
+
+#endif
