51929575ab7988bc7e90c3f301a9157197e082f1
Ryan Petrich <rpetrich@gmail.com>
2010-04-15 22:35:52 -0600

    Added AppleJPEGService

---
+++ AppleJPEGService/AppleJPEGService.h
@@ -0,0 +1,27 @@
+#ifndef _APPLEJPEGSERVICE_H
+#define _APPLEJPEGSERVICE_H 1
+
+#include <IOSurface/IOSurface.h>
+#include <IOKit/IOKitLib.h>
+
+#define kAppleJPEGServiceName "AppleJPEGDriver"
+
+#define kAppleJPEGInitializeDecoder 0
+#define kAppleJPEGStartDecoder      1
+#define kAppleJPEGInitializeEncoder 2
+#define kAppleJPEGStartEncoder      3
+
+struct JPEGDriverArgs {
+	int must_be_zero_1;
+	IOSurfaceID src_surface /*in*/;
+	size_t src_size /*in*/;
+	int must_be_zero_2;
+	IOSurfaceID dest_surface /*in*/;
+	size_t dest_size /*in*/;
+	size_t result_size /*out*/;
+	size_t dest_width /*in*/;
+	size_t dest_height /*in*/;
+	int quality /*in*/;
+};
+
+#endif
