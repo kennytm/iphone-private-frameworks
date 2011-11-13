8b31babf6777fe8a3eb85d0de7f5f79b26e1de5f
Ryan Petrich <rpetrich@gmail.com>
2011-11-13 02:52:21 -0700

    Updated Activator header

--- libactivator/libactivator.h
+++ libactivator/libactivator.h
@@ -22,24 +22,50 @@
 
 @end
 
+@protocol LAEventDataSource <NSObject>
+
+@required
+- (NSString *)localizedTitleForEventName:(NSString *)eventName;
+- (NSString *)localizedGroupForEventName:(NSString *)eventName;
+- (NSString *)localizedDescriptionForEventName:(NSString *)eventName;
+
+@optional
+- (BOOL)eventWithNameIsHidden:(NSString *)eventName;
+- (BOOL)eventWithName:(NSString *)eventName isCompatibleWithMode:(NSString *)eventMode;
+
+@end
+
+typedef enum {
+    LAActivatorVersion_1_3 = 1030000,
+    LAActivatorVersion_1_4 = 1040000,
+    LAActivatorVersion_1_5 = 1050000,
+    LAActivatorVersion_1_5_1 = 1050100,
+    LAActivatorVersion_1_5_2 = 1050200,
+    LAActivatorVersion_1_5_3 = 1050300,
+    LAActivatorVersion_1_5_4 = 1050400,
+    LAActivatorVersion_1_5_5 = 1050500,
+    LAActivatorVersion_1_5_7 = 1050700,
+    LAActivatorVersion_1_5_8 = 1050800,
+} LAActivatorVersion;
+
 // Activator
 
 @protocol LAListener;
 
 @interface LAActivator : NSObject {
 @private
-	NSMutableDictionary *_listeners;
-	NSMutableDictionary *_preferences;
-	NSUInteger _suppressReload;
-	NSMutableDictionary *_eventData;
-	NSBundle *_mainBundle;
-	NSDictionary *_cachedAndSortedListeners;
+	NSArray *_availableEventModes;
+@protected
 	NSMutableDictionary *_cachedListenerTitles;
 	NSMutableDictionary *_cachedListenerGroups;
+	NSMutableDictionary *_cachedListenerSmallIcons;
+	CFMutableSetRef _listenerInstances;
 }
 + (LAActivator *)sharedInstance;
 
-@property (nonatomic, readonly) NSString *settingsFilePath;
+@property (nonatomic, readonly) LAActivatorVersion version;
+@property (nonatomic, readonly, getter=isRunningInsideSpringBoard) BOOL runningInsideSpringBoard;
+@property (nonatomic, readonly, getter=isDangerousToSendEvents) BOOL dangerousToSendEvents;
 
 - (id<LAListener>)listenerForEvent:(LAEvent *)event;
 - (void)sendEventToListener:(LAEvent *)event;
@@ -62,6 +88,9 @@
 - (NSArray *)compatibleModesForEventWithName:(NSString *)name;
 - (BOOL)eventWithName:(NSString *)eventName isCompatibleWithMode:(NSString *)eventMode;
 
+- (void)registerEventDataSource:(id<LAEventDataSource>)dataSource forEventName:(NSString *)eventName;
+- (void)unregisterEventDataSourceWithEventName:(NSString *)eventName;
+
 @property (nonatomic, readonly) NSArray *availableListenerNames;
 - (id)infoDictionaryValueOfKey:(NSString *)key forListenerWithName:(NSString *)name;
 - (BOOL)listenerWithNameRequiresAssignment:(NSString *)name;
@@ -72,7 +101,7 @@
 - (UIImage *)smallIconForListenerName:(NSString *)listenerName;
 
 @property (nonatomic, readonly) NSArray *availableEventModes;
-- (NSString *)currentEventMode;
+@property (nonatomic, readonly) NSString *currentEventMode;
 
 @end
 
@@ -103,14 +132,12 @@ extern LAActivator *LASharedActivator;
 // Incoming events
 - (void)activator:(LAActivator *)activator receiveEvent:(LAEvent *)event forListenerName:(NSString *)listenerName;
 - (void)activator:(LAActivator *)activator abortEvent:(LAEvent *)event forListenerName:(NSString *)listenerName;
-- (void)activator:(LAActivator *)activator otherListenerDidHandleEvent:(LAEvent *)event forListenerName:(NSString *)listenerName;
-- (void)activator:(LAActivator *)activator receiveDeactivateEvent:(LAEvent *)event forListenerName:(NSString *)listenerName;
+- (void)activator:(LAActivator *)activator receiveDeactivateEvent:(LAEvent *)event;
+- (void)activator:(LAActivator *)activator otherListenerDidHandleEvent:(LAEvent *)event;
 
 // Simpler versions
 - (void)activator:(LAActivator *)activator receiveEvent:(LAEvent *)event;
 - (void)activator:(LAActivator *)activator abortEvent:(LAEvent *)event;
-- (void)activator:(LAActivator *)activator otherListenerDidHandleEvent:(LAEvent *)event;
-- (void)activator:(LAActivator *)activator receiveDeactivateEvent:(LAEvent *)event;
 
 // Metadata (may be cached)
 - (NSString *)activator:(LAActivator *)activator requiresLocalizedTitleForListenerName:(NSString *)listenerName;
@@ -119,7 +146,9 @@ extern LAActivator *LASharedActivator;
 - (NSNumber *)activator:(LAActivator *)activator requiresRequiresAssignmentForListenerName:(NSString *)listenerName;
 - (NSArray *)activator:(LAActivator *)activator requiresCompatibleEventModesForListenerWithName:(NSString *)listenerName;
 - (NSData *)activator:(LAActivator *)activator requiresIconDataForListenerName:(NSString *)listenerName;
+- (NSData *)activator:(LAActivator *)activator requiresIconDataForListenerName:(NSString *)listenerName scale:(CGFloat *)scale;
 - (NSData *)activator:(LAActivator *)activator requiresSmallIconDataForListenerName:(NSString *)listenerName;
+- (NSData *)activator:(LAActivator *)activator requiresSmallIconDataForListenerName:(NSString *)listenerName scale:(CGFloat *)scale;
 - (NSNumber *)activator:(LAActivator *)activator requiresIsCompatibleWithEventName:(NSString *)eventName listenerName:(NSString *)listenerName;
 - (id)activator:(LAActivator *)activator requiresInfoDictionaryValueOfKey:(NSString *)key forListenerWithName:(NSString *)listenerName;
 
@@ -127,7 +156,55 @@ extern LAActivator *LASharedActivator;
 
 // Settings Controller
 
-@interface LAListenerSettingsViewController : UIViewController {
+@protocol LASettingsViewControllerDelegate;
+
+@interface LASettingsViewController : UIViewController {
+@private
+	UITableView *_tableView;
+	id<LASettingsViewControllerDelegate> _delegate;
+	UINavigationController *_savedNavigationController;
+}
+
++ (id)controller;
+- (id)init;
+
+@end
+
+@interface LARootSettingsController : LASettingsViewController {
+}
+@end
+
+@interface LAModeSettingsController : LASettingsViewController {
+@private
+	NSString *_eventMode;
+	NSMutableDictionary *_events;
+	NSArray *_groups;
+}
+
+- (id)initWithMode:(NSString *)mode;
+
+@end
+
+@class ActivatorEventViewHeader;
+@class LAListenerTableViewDataSource;
+
+@interface LAEventSettingsController : LASettingsViewController {
+@private
+	NSArray *_modes;
+	NSMutableSet *_currentAssignments;
+	NSString *_eventName;
+	LAListenerTableViewDataSource *_dataSource;
+	ActivatorEventViewHeader *_headerView;
+	UISearchBar *_searchBar;
+	UIView *_headerWrapper;
+}
+
+- (id)initWithModes:(NSArray *)modes eventName:(NSString *)eventName;
+
+@end
+
+
+@interface LAListenerSettingsViewController : LASettingsViewController {
 @private
 	NSString *_listenerName;
 	NSString *_eventMode;
@@ -136,7 +213,6 @@ extern LAActivator *LASharedActivator;
 	NSArray *_groups;
 }
 
-- (id)init;
 @property (nonatomic, copy) NSString *listenerName;
 
 @end
@@ -160,17 +236,29 @@ extern NSString * const LAEventNameStatusBarSwipeRight;
 extern NSString * const LAEventNameStatusBarSwipeLeft;
 extern NSString * const LAEventNameStatusBarSwipeDown;
 extern NSString * const LAEventNameStatusBarTapDouble;
+extern NSString * const LAEventNameStatusBarTapSingle;
 extern NSString * const LAEventNameStatusBarHold;
 
 extern NSString * const LAEventNameVolumeDownUp;
 extern NSString * const LAEventNameVolumeUpDown;
 extern NSString * const LAEventNameVolumeDisplayTap;
+extern NSString * const LAEventNameVolumeToggleMuteTwice;
+extern NSString * const LAEventNameVolumeDownHoldShort;
+extern NSString * const LAEventNameVolumeUpHoldShort;
+extern NSString * const LAEventNameVolumeDownPress;
+extern NSString * const LAEventNameVolumeUpPress;
+extern NSString * const LAEventNameVolumeBothPress;
 
 extern NSString * const LAEventNameSlideInFromBottom;
 extern NSString * const LAEventNameSlideInFromBottomLeft;
 extern NSString * const LAEventNameSlideInFromBottomRight;
 
 extern NSString * const LAEventNameMotionShake;
+
+extern NSString * const LAEventNameHeadsetButtonPressSingle;
+extern NSString * const LAEventNameHeadsetButtonHoldShort;
+
+extern NSString * const LAEventNameLockScreenClockDoubleTap;
 
 @interface LASimpleListener : NSObject<LAListener> {
 }
